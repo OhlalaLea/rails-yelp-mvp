24d00be039e46295d7e2e42cd0197ec052df4b72
lea saulnier <leasaulnier@orange.fr>
2023-05-24 15:01:16 +0400

    seed 5 restos ok

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,19 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+require 'faker'
+
+CATEGORIES = %w[chinese italian japanese french belgian].freeze
+
+puts 'Creating 5 fake restaurants...'
+5.times do
+  restaurant = Restaurant.new(
+    name: Faker::Restaurant.name,
+    address: Faker::Address.street_address,
+    phone_number: Faker::PhoneNumber.cell_phone,
+    category: CATEGORIES.sample
+  )
+  restaurant.save!
+end
+puts 'Finished!'
